c08b274feab9d36c0977a9e722f1ecec478423fc
idkbutimlucas <76565606+idkbutimlucas@users.noreply.github.com>
2022-03-07 17:49:00 +0100

    postman test

---
+++ routes/students.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+var student_Controller = require("../controllers/student");
+
+router.get("/", student_Controller.getAll);
+
+router.get("/:id", student_Controller.getById);
+
+router.post("/", student_Controller.create);
+
+router.put("/;id", student_Controller.update);
+
+router.delete("/:id", student_Controller.delete);
+
+module.exports = router;
